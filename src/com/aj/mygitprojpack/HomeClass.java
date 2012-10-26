5f6e56f988b254e02103f0664830e23b15eed3df
Administrator <Administrator@aedapala.in.ibm.com>
2012-10-26 11:19:04 -0400

    second comit.

--- src/com/aj/mygitprojpack/HomeClass.java
+++ src/com/aj/mygitprojpack/HomeClass.java
@@ -3,5 +3,7 @@ package com.aj.mygitprojpack;
 public class HomeClass {
 	public static void main(String args[]){
 		System.out.println("Hi There.........");
+		System.out.println("Hi There.........1");
+		System.out.println("Hi There.........2");
 	}
 }
